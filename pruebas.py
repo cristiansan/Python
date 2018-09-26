bf4d02351629c6a5ff25983c9c0eb4ce8034006b
cristiansan <cristiansan@gmail.com>
2018-09-26 12:05:49 -0300

    primer commit

---
+++ pruebas.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+print("Test")
+
+animals = open("text.txt", "r+")
+text = animals.read()
+#print(text)
+animals.seek(0)
+for animal in animals:
+    print(animal)
+
+animals.write("tigre\n")
+animals.write("puma\n")
+
+animals.close()
+
+
+#lista y dicc
+"""dicc = {}
+dicc["nombre"]="Cris"
+dicc["ciudad"]="capital"
+dicc["edad"]=41
+print (dicc["nombre"], dicc["edad"])
+dicc["nombre"]="Cristian"
+
+for k, v in dicc.items():
+    print(k,"=>", v)"""
+
+
+#while
+"""age=18
+while age < 30:
+    print ("no tan grande",age)
+    age += 1"""
+
+#select 
+"""name="test 1234"
+first=name[0:7]
+print(first)"""
+#test
+"""frase=input("Ingrese: ")
+a=frase
+print(a)"""
+# python
